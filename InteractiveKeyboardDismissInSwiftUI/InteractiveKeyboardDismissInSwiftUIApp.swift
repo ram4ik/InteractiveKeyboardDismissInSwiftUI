e40468ca7f6dbdd84fbaf4ac15e0c8ecd069a7d2
Ramill <ramill_ibragimov@icloud.com>
2022-10-05 13:19:55 +0300

    Initial Commit

---
+++ InteractiveKeyboardDismissInSwiftUI/InteractiveKeyboardDismissInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveKeyboardDismissInSwiftUIApp.swift
+//  InteractiveKeyboardDismissInSwiftUI
+//
+//  Created by Ramill Ibragimov on 05.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveKeyboardDismissInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
