a282773ff53acc3dcb57948b70aee3847ec142a4
Ramill <ramill_ibragimov@icloud.com>
2022-10-05 13:26:56 +0300

    init

--- InteractiveKeyboardDismissInSwiftUI/ContentView.swift
+++ InteractiveKeyboardDismissInSwiftUI/ContentView.swift
@@ -8,12 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var input = "Use an AnyLayout instance to enable dynamically changing the type of a layout container without destroying the state of the subviews. For example, you can create a layout that changes between horizontal and vertical layouts based on the current Dynamic Type setting:"
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            ScrollView {
+                TextEditor(text: $input)
+                    .frame(height: 400)
+            }
+            .scrollDismissesKeyboard(.immediately)
         }
         .padding()
     }
